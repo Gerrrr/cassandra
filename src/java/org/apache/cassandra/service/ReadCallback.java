0baaae4c7ce2a3e1035b3461f73cf2a38d519ce5
Robert Stupp <snazy@snazy.de>
2017-11-14 09:41:09 +0100

    don't log DigestMismatchException at DEBUG
    patch by Robert Stupp; reviewed by Andres de la Pena Garcia

--- src/java/org/apache/cassandra/service/ReadCallback.java
+++ src/java/org/apache/cassandra/service/ReadCallback.java
@@ -238,8 +238,6 @@ public class ReadCallback implements IAsyncCallbackWithFailure<ReadResponse>
 
                 if (traceState != null)
                     traceState.trace("Digest mismatch: {}", e.toString());
-                if (logger.isDebugEnabled())
-                    logger.debug("Digest mismatch:", e);
 
                 ReadRepairMetrics.repairedBackground.mark();
 
